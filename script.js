da32d9b40d072afd7abd18af71528f475631af1f
Adwik87 <pandeyadwik@gmail.com>
2026-03-13 18:24:38 +0530

    Might give up after this but we'll see

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
-console.log("Portfolio loaded");
+const button = document.getElementById("enter-btn");
+const intro = document.getElementById("intro-screen");
+
+button.addEventListener("click", function(){
+intro.style.display = "none";
+});
